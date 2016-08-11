83838a29534631386b2f7caff6f00667f33c51bb
dreamer2018 <zhoupans_mail@163.com>
2016-08-11 09:09:51 +0800

    对部分样式做了更改

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,23 @@
+// JavaScript Document
+$(function () {
 
-   // JavaScript Document
-$(function(){
-	//var pagename=$(".postiton span a:last").html();
-	var pagename2=$(".fwmain_nright .label .label_title").html();
-	//alert(pagename2);
-	$("#ulMenu li a").removeClass("cur");
-	
-	if(pagename2=="关于我们"){
-		$("#ulMenu li:eq(1) a").addClass("cur");
-	}else if(pagename2=="新闻动态"){
-		$("#ulMenu li:eq(2) a").addClass("cur");
-	}else if(pagename2=="专题活动"){
-		$("#ulMenu li:eq(3) a").addClass("cur");
-	}else if(pagename2=="公益招募"){
-		$("#ulMenu li:eq(4) a").addClass("cur");
-	}else if(pagename2=="乐捐"){
-		$("#ulMenu li:eq(5) a").addClass("cur");
-	}else if(pagename2=="关于我们"){
-		$("#ulMenu li:eq(6) a ").addClass("cur");
-	}else {
-		$("#ulMenu li:eq(0) a").addClass("cur");
-	}
+   // var pagename2 = $(".fwmain_nright .label .label_title").html();
+    var pagename2 = $(".current_name p").html();
+    $("#ulMenu li a").removeClass("cur");
+ //   alert(pagename2)
+    if (pagename2 == "新闻动态") {
+        $("#ulMenu li:eq(1) a").addClass("cur");
+    } else if (pagename2 == "专题活动") {
+        $("#ulMenu li:eq(2) a").addClass("cur");
+    } else if (pagename2 == "公益招募") {
+        $("#ulMenu li:eq(3) a").addClass("cur");
+    } else if (pagename2 == "乐捐") {
+        $("#ulMenu li:eq(4) a").addClass("cur");
+    } else if (pagename2 == "关于我们") {
+        $("#ulMenu li:eq(5) a ").addClass("cur");
+    } else if (pagename2 == "联系我们") {
+        $("#ulMenu li:eq(6) a").addClass("cur");
+    } else {
+        $("#ulMenu li:eq(0) a").addClass("cur");
+    }
 });
