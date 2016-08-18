48e4668148ecb2b9f5868b02198f42320601f45d
dreamer2018 <zhoupans_mail@163.com>
2016-08-18 09:00:33 +0800

    增加注册信息

--- login/models.py
+++ login/models.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+    Create By zhoupan At 2016.08.17
+"""
+
 from django.db import models
 
+
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=20)  # 用户名
+    phone = models.CharField(max_length=11, unique=True)  # 手机号码
+    passwd = models.CharField(max_length=20)  # 密码
+    birthday = models.CharField(max_length=10, blank=True)  # 生日
+    address = models.CharField(max_length=40, blank=True)  # 地址
+    about = models.TextField()  # 个人说明
--- login/views.py
+++ login/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from django.shortcuts import render_to_response
+
 
 # Create your views here.
+# 登录页面
+def login(request):
+    return render_to_response('login.html', {
+        'url': '../static/',
+    }
+                              )
+
+
+# 注册页面
+def register(request):
+    return render_to_response('register.html', {
+        'url': '../static/'
+    }
+                              )
